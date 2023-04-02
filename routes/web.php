23d862ac4cc202930eb89d48d2b8f10623112830
Zulfiqry Saadputra <zsp301@gmail.com>
2023-04-03 01:43:45 +0800

    Change default route

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/','login');
 
 Route::get('/home', function () {
     return view('home');
