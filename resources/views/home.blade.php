fe2b8708817acd37d3e0d1a0f0e4351aa40972a8
Zulfiqry Saadputra <zsp301@gmail.com>
2023-04-19 03:49:48 +0800

    adjust home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,53 +3,55 @@
     <section class="flex flex-col w-full shadow-md">
         <div class="relative">
             <div
-                class="flex justify-center mx-auto px-12 sm:px-10 lg:px-20 pt-10 pb-24 z-10 h-[960px] sm:h-[880px] bg-[url('/public/assets/image/header-bg.png')] bg-cover bg-no-repeat">
+                class="flex justify-center mx-auto px-12 sm:px-10 lg:px-20 pt-10 pb-24 z-10 sm:h-[880px] bg-[url('/public/assets/image/header-bg.png')] bg-cover bg-no-repeat">
                 <div class="flex sm:mt-14 max-w-6xl justify-center">
                     <div class="flex flex-col sm:flex-row gap-20 pb-7">
-                        <div class="flex flex-col gap-10">
+                        <div class="flex flex-col gap-3">
                             <div class="w-full">
-                                <h2 class="text-4xl font-normal text-white md:text-xl">Selamat Datang Di Portal Alumni
+                                <h2 class="mb-2 text-xl font-light text-white md:text-2xl">
+                                    Selamat Datang Di Portal Alumni,
                                 </h2>
-                                <h2 class="mb-8 text-4xl font-semibold tracking-widest text-white uppercase title-font">
-                                    {{ Auth::user()->name }}! </h2>
+                                <h2 class="text-4xl max-w-[480px] font-semibold tracking-widest text-white uppercase title-font">
+                                    {{ Auth::user()->name }}!</h2>
                             </div>
-                            <div class="flex gap-4">
+                            <p class="max-w-[480px] text-lg font-light text-gray-300 md:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing
+                                elit. Odit ipsa consequatur, illum est cum, expedita
+                                repudiandae accusamus nesciunt inventore architecto</p>
+                            <div class="lg:mt-20 flex flex-col gap-4">
                                 <div class="flex flex-col gap-2">
-                                    <h2 class="text-4xl font-bold leading-tight lg:text-5xl text-white">Get in touch
+                                    <h2 class="text-3xl font-semibold leading-tight lg:text-5xl text-white">Get in touch
                                     </h2>
-                                    <span class="text-lg text-white">Saling Terhubung dengan Para Alumni</span>
+                                    <span class="text-lg md:text-2xl font-light text-white">Saling Terhubung dengan Para Alumni</span>
                                 </div>
                                 <a href="{{ route('database') }}"
-                                    class="btn transition-colors bg-cyan-800 self-center rounded-md w-40 h-10 text-white text-hover shadow-lg">
-                                    Here
+                                    class="btn normal-case font-semibold text-lg transition-colors bg-cyan-800 rounded-md w-40 h-10 text-white text-hover drop-shadow-xl">
+                                    Cari Alumni
                                 </a>
                             </div>
                         </div>
                         <div class="flex flex-col items-center">
-                            <div
-                                class="!z-5 relative flex flex-col rounded-[20px] max-w-[300px] bg-clip-border shadow-3xl shadow-shadow-500  w-full !p-4 3xl:p-![18px] bg-white undefined bg-opacity-60">
-                                <div class="h-full w-full">
-                                    <div class="relative w-full">
+                            <div class="!z-5 mb-20 w-full p-4 3xl:p-![18px] rounded-[20px] max-w-[300px] bg-clip-border shadow-3xl shadow-shadow-500 bg-white bg-opacity-60">
+                                <div class="flex flex-col">
+                                    <div class="relative aspect-square">
                                         <img src="{{ $user->profile_photo_url }}"
-                                            class="mb-3 h-full w-full rounded-xl 3xl:h-full 3xl:w-full" alt="">
+                                            class="object-cover mb-3 h-full w-full rounded-xl 3xl:h-full 3xl:w-full" alt="foto-user">
                                     </div>
-                                    <div class="mb-3 flex items-center justify-between px-1 md:items-start">
-                                        <div class="mb-1">
-                                            <p class="text-lg font-bold text-navy-700"> {{ Auth::user()->name }} </p>
-                                            <p class="mt-1 text-sm font-medium text-gray-600 md:mt-2">UX Researcher,
+                                    <div class="my-3 px-1 md:items-start">
+                                        <div class="mb-1 flex flex-col justify-left">
+                                            <p class="text-xl font-bold text-navy-700"> {{ Auth::user()->name }} </p>
+                                            <div class="flex gap-2 items-center">
+                                                <i class="iconoir-calendar text-sm text-gray-600"></i>
+                                                <span class="text-base text-gray-600"> {{ Auth::user()->jurusan }} </span>
+                                            </div>
+                                            <p class="text-sm font-medium text-gray-600">UX Researcher,
                                                 Co-host of the Interesting Design podcast
                                                 and a proud mother of three children </p>
                                         </div>
                                     </div>
-                                    <div class="flex items-center justify-between md:items-center lg:justify-between ">
-                                        <ul
-                                            class="space-y-4 md:space-y-0 space-x-0 md:space-x-4 flex flex-col md:flex-row text-left justify-center pt-1">
-                                            <li class="flex items-center text-sm"><i
-                                                    class="iconoir-calendar mr-2"></i>Teknik Informatika 1990 </li>
-                                        </ul>
-                                        <button href=""
-                                            class="linear rounded-[20px] bg-red-900 px-4 py-2 text-base font-medium text-white transition duration-200 hover:bg-brand-800 active:bg-brand-700 ju">Perbarui
-                                            Profl</button>
+                                    <div class="flex items-center">
+                                        <a href="{{ route('profile.show') }}"
+                                            class=" btn rounded-lg min-h-[40px] h-fit bg-red-800 normal-case border-none text-sm font-medium text-white shadow-md">Perbarui
+                                            Profil</a>
                                     </div>
 
                                 </div>
