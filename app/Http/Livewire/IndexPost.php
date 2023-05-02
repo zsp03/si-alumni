7e34eb9192c3ca2039ad58960e9ffc3394319e55
Zulfiqry Saadputra <zsp301@gmail.com>
2023-05-02 15:02:58 +0800

    Load more on event page, change card styling

--- app/Http/Livewire/IndexPost.php
+++ app/Http/Livewire/IndexPost.php
@@ -7,10 +7,27 @@ use Livewire\Component;
 
 class IndexPost extends Component
 {
+    public $perPage = 4;
+    public $postsCount;
+    protected $listeners = [
+      'increasePerPage'
+    ];
+    public function increasePerPage($count)
+    {
+        $this->perPage += $count;
+    }
+    public function mount(Post $post)
+    {
+        $this->postsCount = $post->all()->count();
+    }
+    public function loadMore($counts)
+    {
+        $this->emit('increasePerPage', $counts);
+    }
     public function render()
     {
         return view('livewire.index-post',[
-            'dataPosts' => Post::all()
+            'dataPosts' => Post::all()->paginate($this->perPage)
         ]);
     }
 }
--- resources/views/components/event-post-card.blade.php
+++ resources/views/components/event-post-card.blade.php
@@ -4,16 +4,20 @@
     'post'
 ])
 
-<a rel="noopener noreferrer" href="{{ $href }}" {{ $attributes->merge(['class' => 'border border-gray-200 animate-in slide-in-from-right duration-700 bg-white shadow-md rounded-md max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900']) }}>
-    <img alt="featured-image" role="presentation" class="object-cover w-full rounded-t-md @if($size == 'big') lg:rounded-t-none lg:rounded-l-md h-64 sm:h-96 lg:col-span-7 @else h-44 @endif dark:bg-gray-500" src="{{ $post->image_featured_url !== null ? $post->image_featured_url : 'https://source.unsplash.com/random/480x360?'. rand(1,50) }}">
-    <div class="flex flex-col p-5 @if($size == 'big') lg:col-span-5 @endif">
-        <h3 class="text-2xl font-semibold @if($size == 'big') sm:text-4xl @endif group-hover:underline group-focus:underline">{{ $post->title }}</h3>
+<div {{ $attributes->merge(['class' => 'duration-300 ease-in hover:scale-[1.01] border border-gray-200 bg-white shadow-md rounded-md max-w-sm mx-auto hover:shadow-lg hover:shadow-gray-600 group-hover:no-underline focus:no-underline dark:bg-gray-900']) }}>
+    <a href="{{ $href }}" class="lg:col-span-7">
+        <img alt="featured-image" role="presentation" class="object-cover w-full rounded-t-md @if($size == 'big') lg:rounded-tr-none lg:rounded-l-md h-64 sm:h-96 @else h-44 @endif dark:bg-gray-500" src="{{ $post->image_featured_url !== null ? $post->image_featured_url : 'https://source.unsplash.com/random/480x360?'. rand(1,50) }}">
+    </a>
+    <div class="my-auto w-fit flex flex-col p-5 @if($size == 'big') lg:col-span-5 @endif">
+        <a rel="noopener noreferrer" href="{{ $href }}" class="w-fit">
+            <h3 class="text-2xl font-semibold text-gray-900 hover:drop-shadow-lg @if($size == 'big') sm:text-4xl @endif">{{ $post->title }}</h3>
+        </a>
         <span class="text-sm text-gray-600 @if($size == 'big') sm:text-lg @endif dark:text-gray-400">{{\Carbon\Carbon::parse($post->updated_at)->translatedFormat('F d, Y')}}</span>
-        <div class="flex gap-2">
+        <div class="mt-1 flex gap-2">
             @foreach($post->tags as $tag)
-                <x-wireui.badge primary label="{{ $tag->name }}"/>
+                <x-wireui.badge primary label="{{ $tag->name }}" class="hover:bg-black"/>
             @endforeach
         </div>
         <p class="mt-2">{{ $post->shortContent() }}</p>
     </div>
-</a>
+</div>
