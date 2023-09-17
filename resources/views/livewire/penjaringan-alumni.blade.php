3d919f5e860e1f07303cd6f61ee20918eddde5d7
Fdhlu-Rhmn <fadhlurahmanf10@gmail.com>
2023-09-18 01:02:31 +0800

    Memperbaiki form penjaring alumni,  penambahan tahun masuk dan tahun lulus

--- resources/views/livewire/penjaringan-alumni.blade.php
+++ resources/views/livewire/penjaringan-alumni.blade.php
@@ -2,32 +2,43 @@
     <div class=" max-w-4xl w-full rounded overflow-hidden shadow-lg px-5 py-5 md:mx-auto border">
         <form wire:submit.prevent="addData">
             @csrf
+            <div class="mx-5">
+                <label for="nama" class="block mb-2 text-sm font-semibold text-red-500 dark:text-white"><span class="text-red-500"> *</span>Form wajib diisi</label>
+            </div>
             <div class="mb-6">
-                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Lengkap</label>
+                <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Lengkap <span class="text-red-500"> *</span></label>
                 <input wire:model="name" type="nama" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nama Lengkap" >
-            </div>
-            <div class="mb-6">
-                <label for="jenis_kelamin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Kelamin</label>
-                <input wire:model="jenis_kelamin" type="jenis_kelamin" id="jenis_kelamin" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Jenis Kelamin" >
-            </div>
-            <div class="mb-6">
-                <label for="program_studi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Program Studi/jurusan</label>
-                <input wire:model="program_studi" type="program_studi" id="program_studi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Program Studi/jurusan" >
-            </div>
-            <div class="mb-6">
-                <label for="fakultas" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fakultas</label>
-                <input wire:model="fakultas" type="nama" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Fakultas" >
             </div>
             <div class="mb-6">
                 <label for="nim" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIM</label>
                 <input wire:model="nim" type="nim" id="nim" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="NIM" >
             </div>
             <div class="mb-6">
+                <label for="fakultas" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fakultas<span class="text-red-500"> *</span></label>
+                <input wire:model="fakultas" type="nama" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Fakultas" >
+            </div>
+            <div class="mb-6">
+                <label for="program_studi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Program Studi/jurusan<span class="text-red-500"> *</span></label>
+                <input wire:model="program_studi" type="program_studi" id="program_studi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Program Studi/jurusan" >
+            </div>
+            <div class="mb-6">
+                <label for="tahun_masuk" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tahun masuk<span class="text-red-500"> *</span></label>
+                <input wire:model="tahun_masuk" type="tahun_masuk" id="tahun_masuk" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tahun Masuk" >
+            </div>
+            <div class="mb-6">
+                <label for="tahun_lulus" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tahun masuk<span class="text-red-500"> *</span></label>
+                <input wire:model="tahun_lulus" type="tahun_lulus" id="tahun_lulus" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tahun Masuk" >
+            </div>
+            <div class="mb-6">
+                <label for="jenis_kelamin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Jenis Kelamin</label>
+                <input wire:model="jenis_kelamin" type="jenis_kelamin" id="jenis_kelamin" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Jenis Kelamin" >
+            </div>
+            <div class="mb-6">
                 <label for="phone_number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">No. Handphone(Whatsapp)</label>
                 <input wire:model="phone_number" type="phone_number" id="phone_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="No. Handphone" >
             </div>
             <div class="mb-6">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email<span class="text-red-500"> *</span></label>
                 <input wire:model="email" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Alamat Email" >
             </div>
             <div class="mb-6">
